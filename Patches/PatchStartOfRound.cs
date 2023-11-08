eaace9dc416ef17bba5eb51c38f06ab636f64ab0
Simulator Haven <10366089+Simulator-Haven@users.noreply.github.com>
2023-11-07 20:43:51 -0500

    Screen Text Modifier
    Added a modifier for the screen text that's displayed when a player lands.

--- Patches/PatchStartOfRound.cs
+++ Patches/PatchStartOfRound.cs
@@ -12,4 +12,11 @@ public class PatchStartOfRound
         __instance.overrideRandomSeed = true;
         __instance.overrideSeedNumber = 123456789;
     }
+    
+    [HarmonyPatch("StartGame")]
+    [HarmonyPostfix]
+    public static void SetScreenLevelDescription(StartOfRound __instance)
+    {
+        __instance.screenLevelDescription.text = "Moon of the Day";
+    }
 }
