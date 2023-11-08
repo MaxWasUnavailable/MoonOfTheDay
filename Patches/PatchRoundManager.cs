744f41e02000d4cb037fdea6d5f8d62c94e2bbe7
Simulator Haven <10366089+Simulator-Haven@users.noreply.github.com>
2023-11-07 22:08:12 -0500

    Scrap Location Locked to Seed
    Created a method which overrides the random placement of scrap to be deterministic.

---
+++ Patches/PatchRoundManager.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace MoonOfTheDay.Patches;
+
+[HarmonyPatch(typeof(RoundManager))]
+public class PatchRoundManager
+{
+    [HarmonyPatch("GetRandomNavMeshPositionInRadiusSpherical")]
+    [HarmonyPrefix]
+    public static bool GetRandomNavMeshPositionInRadiusSpherical(RoundManager __instance, ref Vector3 __result, Vector3 pos, float radius = 10f,
+        NavMeshHit navHit = default)
+    {
+        Vector3 randomVector = new Vector3((float)(__instance.LevelRandom.NextDouble() * 2 - 1), (float)(__instance.LevelRandom.NextDouble() * 2 - 1),
+            (float)(__instance.LevelRandom.NextDouble() * 2 - 1));
+        pos = randomVector * radius + pos;
+        __result = NavMesh.SamplePosition(pos, out navHit, radius, -1) ? navHit.position : pos;
+        return false;
+    }
+}
