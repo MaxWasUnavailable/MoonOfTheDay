81416f42ef16de5665caaf208e577456d73c4076
Simulator Haven <10366089+Simulator-Haven@users.noreply.github.com>
2023-11-07 20:19:41 -0500

    Main Code Initial Commit
    Adding in the patch to StartGame method to insert code into StartOfRound where we override the seed.

---
+++ Patches/PatchStartOfRound.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace MoonOfTheDay.Patches;
+
+[HarmonyPatch(typeof(StartOfRound))]
+public class PatchStartOfRound
+{
+    [HarmonyPatch("StartGame")]
+    [HarmonyPrefix]
+    public static void SetSeed(StartOfRound __instance)
+    {
+        __instance.overrideRandomSeed = true;
+        __instance.overrideSeedNumber = 123456789;
+    }
+}
