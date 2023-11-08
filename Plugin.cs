2cab5172de93e62899679df785747c6d14faa0a2
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-08 02:05:32 +0100

    Add initial plugin setup

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,63 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
-namespace MoonOfTheDay
+namespace MoonOfTheDay;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    internal new static ManualLogSource Logger;
+
+    private Harmony _harmony;
+    private bool _isPatched;
+    public static Plugin Instance { get; private set; }
+
+    private void Awake()
     {
-        private void Awake()
+        // Set instance
+        Instance = this;
+
+        // Init logger
+        Logger = base.Logger;
+
+        // Patch using Harmony
+        PatchAll();
+
+        // Report plugin is loaded
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public void PatchAll()
+    {
+        if (_isPatched)
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogWarning("Already patched!");
+            return;
         }
+
+        Logger.LogDebug("Patching...");
+
+        _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll();
+        _isPatched = true;
+
+        Logger.LogDebug("Patched!");
+    }
+
+    public void UnpatchAll()
+    {
+        if (!_isPatched)
+        {
+            Logger.LogWarning("Not patched!");
+            return;
+        }
+
+        Logger.LogDebug("Unpatching...");
+
+        _harmony.UnpatchSelf();
+        _isPatched = false;
+
+        Logger.LogDebug("Unpatched!");
     }
 }
