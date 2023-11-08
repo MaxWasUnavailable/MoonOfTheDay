8dcc73051531e4e28ff1afc3412e28cabdc6d935
Simulator Haven <10366089+Simulator-Haven@users.noreply.github.com>
2023-11-07 22:21:11 -0500

    Make Hazard Spawns Deterministic
    Co-authored-by: Max <89798523+maxwasunavailable@users.noreply.github.com>

--- Patches/PatchRoundManager.cs
+++ Patches/PatchRoundManager.cs
@@ -9,13 +9,38 @@ public class PatchRoundManager
 {
     [HarmonyPatch("GetRandomNavMeshPositionInRadiusSpherical")]
     [HarmonyPrefix]
-    public static bool GetRandomNavMeshPositionInRadiusSpherical(RoundManager __instance, ref Vector3 __result, Vector3 pos, float radius = 10f,
+    public static bool GetRandomNavMeshPositionInRadiusSpherical(RoundManager __instance, ref Vector3 __result,
+        Vector3 pos, float radius = 10f,
         NavMeshHit navHit = default)
     {
-        Vector3 randomVector = new Vector3((float)(__instance.LevelRandom.NextDouble() * 2 - 1), (float)(__instance.LevelRandom.NextDouble() * 2 - 1),
+        var randomVector = new Vector3((float)(__instance.LevelRandom.NextDouble() * 2 - 1),
+            (float)(__instance.LevelRandom.NextDouble() * 2 - 1),
             (float)(__instance.LevelRandom.NextDouble() * 2 - 1));
         pos = randomVector * radius + pos;
         __result = NavMesh.SamplePosition(pos, out navHit, radius, -1) ? navHit.position : pos;
+        return false;
+    }
+
+    [HarmonyPatch("GetRandomNavMeshPositionInRadius")]
+    [HarmonyPrefix]
+    public static bool GetRandomNavMeshPositionInRadius(RoundManager __instance, ref Vector3 __result, Vector3 pos,
+        float radius = 10f,
+        NavMeshHit navHit = default)
+    {
+        var randomVector = new Vector3((float)(__instance.LevelRandom.NextDouble() * 2 - 1),
+            (float)(__instance.LevelRandom.NextDouble() * 2 - 1),
+            (float)(__instance.LevelRandom.NextDouble() * 2 - 1));
+        var y = pos.y;
+        pos = randomVector * radius + pos;
+        pos.y = y;
+        if (NavMesh.SamplePosition(pos, out navHit, radius, -1))
+        {
+            __result = navHit.position;
+            return false;
+        }
+
+        Debug.Log("Unable to get random nav mesh position in radius! Returning old pos");
+        __result = pos;
         return false;
     }
 }
