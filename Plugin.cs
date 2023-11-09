db1f969f71e8e5338a7b579f2dbf3adffd82295d
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-09 06:21:15 +0100

    Add const Daily & Weekly moon names

--- Plugin.cs
+++ Plugin.cs
@@ -5,23 +5,16 @@ using HarmonyLib;
 
 namespace MoonOfTheDay;
 
-public enum MoonType
-{
-    Normal,
-    Daily,
-    Weekly
-}
-
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    public const string DailyMoonName = "Daily Moon";
+    public const string WeeklyMoonName = "Weekly Moon";
     internal new static ManualLogSource Logger;
 
     private Harmony _harmony;
     private bool _isPatched;
     public static Plugin Instance { get; private set; }
-
-    public MoonType SelectedMoonType { get; internal set; } = MoonType.Normal;
 
     private void Awake()
     {
@@ -93,13 +86,13 @@ public class Plugin : BaseUnityPlugin
     public static SelectableLevel GetDailyMoon(SelectableLevel[] moons)
     {
         var random = new Random(GetDailySeed());
-        
+
         var dailyMoon = moons[random.Next(0, moons.Length)];
 
         // Copy the moon to prevent reference issues
         dailyMoon = Instantiate(dailyMoon);
 
-        dailyMoon.PlanetName = "Daily Moon PlanetName";
+        dailyMoon.PlanetName = DailyMoonName;
         dailyMoon.LevelDescription = "This moon looks familiar...";
         dailyMoon.riskLevel = "???";
         dailyMoon.levelID = 999;
@@ -110,13 +103,13 @@ public class Plugin : BaseUnityPlugin
     public static SelectableLevel GetWeeklyMoon(SelectableLevel[] moons)
     {
         var random = new Random(GetWeeklySeed());
-        
+
         var weeklyMoon = moons[random.Next(0, moons.Length)];
 
         // Copy the moon to prevent reference issues
         weeklyMoon = Instantiate(weeklyMoon);
 
-        weeklyMoon.PlanetName = "Weekly Moon PlanetName";
+        weeklyMoon.PlanetName = WeeklyMoonName;
         weeklyMoon.LevelDescription = "This moon looks familiar...";
         weeklyMoon.riskLevel = "???";
         weeklyMoon.levelID = 998;
