c86b7a19996128b9aed8de5cd545d56acc357a5b
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-09 23:58:23 +0100

    Patch both StartOfRound levels & Terminal levels

--- Patches/PatchTerminal.cs
+++ Patches/PatchTerminal.cs
@@ -11,23 +11,37 @@ public class PatchTerminal
     public static void InsertMoons(Terminal __instance)
     {
         Plugin.Logger.LogInfo("Inserting moons...");
+        
+        var startOfRound = StartOfRound.Instance;
 
         var dailyMoon = Plugin.GetDailyMoon(__instance.moonsCatalogueList);
         var weeklyMoon = Plugin.GetWeeklyMoon(__instance.moonsCatalogueList);
         
         var moonList = __instance.moonsCatalogueList.ToList();
+        var levelList = startOfRound.levels.ToList();
 
         moonList.RemoveAll(moon => moon.PlanetName == dailyMoon.PlanetName);
         moonList.RemoveAll(moon => moon.PlanetName == weeklyMoon.PlanetName);
         
-        var highestId = moonList.Max(moon => moon.levelID);
-        
-        dailyMoon.levelID = highestId + 1;
-        weeklyMoon.levelID = highestId + 2;
+        levelList.RemoveAll(level => level.PlanetName == dailyMoon.PlanetName);
+        levelList.RemoveAll(level => level.PlanetName == weeklyMoon.PlanetName);
 
         moonList.Add(dailyMoon);
         moonList.Add(weeklyMoon);
         
+        levelList.Add(dailyMoon);
+        levelList.Add(weeklyMoon);
+        
         __instance.moonsCatalogueList = moonList.ToArray();
+        startOfRound.levels = levelList.ToArray();
+        
+        // Add TerminalNodes for each moon
+        // Find donor TerminalNode for each moon
+        var terminalNodes = __instance.terminalNodes;
+
+        // Part of this can be moved to a StartOfRound patch -- Do need to ensure both lists are in sync
+        //  --> Possibly set them both at StartOfRound Awake instead?
+        //  --> Potential issues when removing & re-adding moon while at said moon?
+        // Need to extract duplicated code to a helper method
     }
 }
