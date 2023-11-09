9fb229328417bbd9fd317c3e3100786e2ec81f73
Simulator Haven <10366089+Simulator-Haven@users.noreply.github.com>
2023-11-08 22:51:02 -0500

    Created Moon Insert Code
    This code removes any old daily/weekly moons and adds the most recent moon for the date.
    
    Co-authored-by: Max <89798523+maxwasunavailable@users.noreply.github.com>

---
+++ Patches/PatchTerminal.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using HarmonyLib;
+
+namespace MoonOfTheDay.Patches;
+
+[HarmonyPatch(typeof(Terminal))]
+public class PatchTerminal
+{
+    [HarmonyPatch("Awake")]
+    [HarmonyPostfix]
+    public static void InsertMoons(Terminal __instance)
+    {
+        var dailyMoon = Plugin.GetDailyMoon();
+
+        var weeklyMoon = Plugin.GetWeeklyMoon();
+
+        var moonList = __instance.moonsCatalogueList.ToList();
+
+        moonList.RemoveAll(moon => moon.PlanetName == dailyMoon.PlanetName);
+        moonList.RemoveAll(moon => moon.PlanetName == weeklyMoon.PlanetName);
+
+        moonList.Add(dailyMoon);
+        moonList.Add(weeklyMoon);
+
+        __instance.moonsCatalogueList = moonList.ToArray();
+    }
+}
