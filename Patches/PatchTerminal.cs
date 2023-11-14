d9fbd50732dfbd6697327122921af8392c742146
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-14 14:20:01 +0100

    Move functionality to helper method to decrease code duplication

--- Patches/PatchTerminal.cs
+++ Patches/PatchTerminal.cs
@@ -5,6 +5,86 @@ using UnityEngine;
 
 namespace MoonOfTheDay.Patches;
 
+internal static class PatchTerminalHelpers
+{
+    public static void AddTerminalCommand(ref Terminal terminal, SelectableLevel moonToAdd)
+    {
+        Plugin.Logger.LogDebug($"Adding terminal command for {moonToAdd.name}...");
+
+        // Find the Confirm, Deny & Route TerminalKeywords
+        var confirmKeyword = terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Confirm");
+        var denyKeyword = terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Deny");
+        var routeKeyword = terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Route");
+
+        // Find the CancelRoute TerminalNode
+        // var cancelRouteNode = Object.FindObjectsOfType<TerminalNode>().First(node => node.name == "CancelRoute");    -- This doesn't work for some reason, but would be less prone to breaking on updates
+        var cancelRouteNode = routeKeyword.compatibleNouns[0].result.terminalOptions[0].result;
+
+        // Create keyword
+        var moonKeyword = ScriptableObject.CreateInstance<TerminalKeyword>();
+        moonKeyword.name = GetKWNameForMoon(moonToAdd);
+        moonKeyword.word = GetKWWordforMoon(moonToAdd);
+        moonKeyword.defaultVerb = routeKeyword;
+        moonKeyword.compatibleNouns = Array.Empty<CompatibleNoun>();
+
+        // Create travel node
+        var travelNode = ScriptableObject.CreateInstance<TerminalNode>();
+        travelNode.name = GetTravelNodeNameForMoon(moonToAdd);
+        travelNode.displayText =
+            "Routing autopilot to " + moonToAdd.name + ".\n\nPlease enjoy your flight.";
+        travelNode.clearPreviousText = true;
+        travelNode.buyRerouteToMoon = moonToAdd.levelID;
+
+        // Create TerminalNode for decision to travel to moon
+        var travelDecisionNode = ScriptableObject.CreateInstance<TerminalNode>();
+        travelDecisionNode.name = Plugin.DailyMoonName;
+        travelDecisionNode.displayText =
+            "The company has detected a rogue planet. It might not be available for long. Do you want to go there?\n\nIt is currently [currentPlanetTime] on this moon.\n\nPlease CONFIRM or DENY.\n\n";
+        travelDecisionNode.clearPreviousText = true;
+        travelDecisionNode.displayPlanetInfo = moonToAdd.levelID;
+        travelDecisionNode.buyRerouteToMoon =
+            -2; // Based on UnityExplorer values. Not sure if this *needs* to be -2. OnSubmit() seems to handle -2 differently.
+        travelDecisionNode.overrideOptions = true;
+        travelDecisionNode.terminalOptions = new[]
+        {
+            new CompatibleNoun { noun = denyKeyword, result = cancelRouteNode },
+            new CompatibleNoun { noun = confirmKeyword, result = travelNode }
+        };
+
+        // Add the TerminalKeyword to the Terminal
+        var allKeywords = terminal.terminalNodes.allKeywords.ToList();
+        allKeywords.Add(moonKeyword);
+        terminal.terminalNodes.allKeywords = allKeywords.ToArray();
+
+        // Expand route keyword to include the moon as compatible noun
+        var compatibleNouns = routeKeyword.compatibleNouns.ToList();
+        compatibleNouns.Add(new CompatibleNoun { noun = moonKeyword, result = travelDecisionNode });
+        routeKeyword.compatibleNouns = compatibleNouns.ToArray();
+
+        // Add the moon name to the Moons TerminalKeyword
+        terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Moons").specialKeywordResult
+                .displayText +=
+            "* " + moonToAdd.name + " [planetTime]\n";
+
+        Plugin.Logger.LogDebug($"Added terminal command for {moonToAdd.name}.");
+    }
+
+    public static string GetKWNameForMoon(SelectableLevel moon)
+    {
+        return "KW" + moon.PlanetName.ToLower().Replace(" ", "-");
+    }
+
+    public static string GetKWWordforMoon(SelectableLevel moon)
+    {
+        return moon.PlanetName.ToLower().Replace(" ", "-");
+    }
+
+    public static string GetTravelNodeNameForMoon(SelectableLevel moon)
+    {
+        return "Travel" + moon.PlanetName.ToLower().Replace(" ", "-");
+    }
+}
+
 [HarmonyPatch(typeof(Terminal))]
 public class PatchTerminal
 {
@@ -12,14 +92,14 @@ public class PatchTerminal
     [HarmonyPostfix]
     public static void InsertMoons(Terminal __instance)
     {
-        Plugin.Logger.LogInfo("Inserting moons in Terminal...");
+        Plugin.Logger.LogDebug("Inserting moons in Terminal...");
 
         var dailyMoon = Plugin.GetDailyMoon(__instance.moonsCatalogueList);
         var weeklyMoon = Plugin.GetWeeklyMoon(__instance.moonsCatalogueList);
 
         var moonList = __instance.moonsCatalogueList.ToList();
 
-        // Remove first to prevent potential issues with duplicate moons
+        // Remove first to prevent potential issues with duplicate moons -- same ID should prevent issues with doing this
         moonList.RemoveAll(moon => moon.PlanetName == dailyMoon.PlanetName);
         moonList.RemoveAll(moon => moon.PlanetName == weeklyMoon.PlanetName);
 
@@ -28,7 +108,7 @@ public class PatchTerminal
 
         __instance.moonsCatalogueList = moonList.ToArray();
     }
-    
+
     [HarmonyPatch("Awake")]
     [HarmonyPostfix]
     public static void AddTerminalCommands(Terminal __instance)
@@ -36,95 +116,20 @@ public class PatchTerminal
         var dailyMoon = Plugin.GetDailyMoon(__instance.moonsCatalogueList);
         var weeklyMoon = Plugin.GetWeeklyMoon(__instance.moonsCatalogueList);
 
-        if (__instance.terminalNodes.allKeywords.Any(keyword => keyword.name == "KW" + Plugin.DailyMoonName.Replace(" ", "-"))) // TODO: This is dirty, the KW name needs to not be hardcoded / better solution must be found.
+        if (__instance.terminalNodes.allKeywords.Any(keyword =>
+                keyword.name == PatchTerminalHelpers.GetKWNameForMoon(dailyMoon)))
         {
-            Plugin.Logger.LogInfo("Terminal nodes have already been modified.");
+            Plugin.Logger.LogDebug("Terminal nodes have already been modified.");
             return;
         }
 
-        Plugin.Logger.LogInfo("Modifying terminal nodes...");
+        Plugin.Logger.LogDebug("Modifying terminal nodes...");
 
-        // Find the Confirm, Deny & Route TerminalKeywords
-        var confirmKeyword = __instance.terminalNodes.allKeywords.First(keyword => keyword.name == "Confirm");
-        var denyKeyword = __instance.terminalNodes.allKeywords.First(keyword => keyword.name == "Deny");
-        var routeKeyword = __instance.terminalNodes.allKeywords.First(keyword => keyword.name == "Route");
+        PatchTerminalHelpers.AddTerminalCommand(ref __instance, dailyMoon);
+        PatchTerminalHelpers.AddTerminalCommand(ref __instance, weeklyMoon);
 
-        // Find the CancelRoute TerminalNode
-        // var cancelRouteNode = Object.FindObjectsOfType<TerminalNode>().First(node => node.name == "CancelRoute");    -- This doesn't work for some reason, but would be less prone to breaking on updates
-        var cancelRouteNode = routeKeyword.compatibleNouns[0].result.terminalOptions[0].result;
-
-        // Create TerminalKeyword for both moons -- This should only be done once -- needs testing if it's removed after a lobby restart, or if we're duplicating it endlessly
-        var dailyMoonKeyword = ScriptableObject.CreateInstance<TerminalKeyword>();
-        dailyMoonKeyword.name = "KW" + Plugin.DailyMoonName.Replace(" ", "-");
-        dailyMoonKeyword.word = Plugin.DailyMoonName.ToLower().Replace(" ", "-");
-        dailyMoonKeyword.defaultVerb = routeKeyword;
-        dailyMoonKeyword.compatibleNouns = Array.Empty<CompatibleNoun>();
-
-        var weeklyMoonKeyword = ScriptableObject.CreateInstance<TerminalKeyword>();
-        weeklyMoonKeyword.name = "KW" + Plugin.WeeklyMoonName.Replace(" ", "-");
-        weeklyMoonKeyword.word = Plugin.WeeklyMoonName.ToLower().Replace(" ", "-");
-        weeklyMoonKeyword.defaultVerb = routeKeyword;
-        weeklyMoonKeyword.compatibleNouns = Array.Empty<CompatibleNoun>();
-
-        // Create TerminalNode for travel to both moons -- This should only be done once -- needs testing to see if it's removed after a lobby restart, or if we're duplicating it endlessly
-        var dailyMoonTravelNode = ScriptableObject.CreateInstance<TerminalNode>();
-        dailyMoonTravelNode.name = "travel" + Plugin.DailyMoonName.Replace(" ", "-");
-        dailyMoonTravelNode.displayText =
-            "Routing autopilot to " + Plugin.DailyMoonName + ".\n\nPlease enjoy your flight.";
-        dailyMoonTravelNode.clearPreviousText = true;
-        dailyMoonTravelNode.buyRerouteToMoon = dailyMoon.levelID;
-
-        var weeklyMoonTravelNode = ScriptableObject.CreateInstance<TerminalNode>();
-        weeklyMoonTravelNode.name = "travel" + Plugin.WeeklyMoonName.Replace(" ", "-");
-        weeklyMoonTravelNode.displayText =
-            "Routing autopilot to " + Plugin.WeeklyMoonName + ".\n\nPlease enjoy your flight.";
-        weeklyMoonTravelNode.clearPreviousText = true;
-        weeklyMoonTravelNode.buyRerouteToMoon = weeklyMoon.levelID;
-
-        // Create TerminalNode for asking to travel to both moons -- This should only be done once -- needs testing to see if it's removed after a lobby restart, or if we're duplicating it endlessly
-        var dailyMoonNode = ScriptableObject.CreateInstance<TerminalNode>();
-        dailyMoonNode.name = Plugin.DailyMoonName;
-        dailyMoonNode.displayText =
-            "The company has detected a rogue planet. It might not be available for long. Do you want to go there?\n\nIt is currently [currentPlanetTime] on this moon.\n\nPlease CONFIRM or DENY.\n\n";
-        dailyMoonNode.clearPreviousText = true;
-        dailyMoonNode.displayPlanetInfo = dailyMoon.levelID;
-        dailyMoonNode.buyRerouteToMoon = -2;
-        dailyMoonNode.overrideOptions = true;
-        dailyMoonNode.terminalOptions = new[]
-        {
-            new CompatibleNoun { noun = denyKeyword, result = cancelRouteNode },
-            new CompatibleNoun { noun = confirmKeyword, result = dailyMoonTravelNode }
-        };
-
-        var weeklyMoonNode = ScriptableObject.CreateInstance<TerminalNode>();
-        weeklyMoonNode.name = Plugin.WeeklyMoonName;
-        weeklyMoonNode.displayText =
-            "The company has detected a rogue planet. It might not be available for long. Do you want to go there?\n\nIt is currently [currentPlanetTime] on this moon.\n\nPlease CONFIRM or DENY.\n\n";
-        weeklyMoonNode.clearPreviousText = true;
-        weeklyMoonNode.displayPlanetInfo = weeklyMoon.levelID;
-        weeklyMoonNode.buyRerouteToMoon = -2;
-        weeklyMoonNode.overrideOptions = true;
-        weeklyMoonNode.terminalOptions = new[]
-        {
-            new CompatibleNoun { noun = denyKeyword, result = cancelRouteNode },
-            new CompatibleNoun { noun = confirmKeyword, result = weeklyMoonTravelNode }
-        };
-
-        // Add the TerminalKeywords to the Terminal
-        var allKeywords = __instance.terminalNodes.allKeywords.ToList();
-        allKeywords.Add(dailyMoonKeyword);
-        allKeywords.Add(weeklyMoonKeyword);
-        __instance.terminalNodes.allKeywords = allKeywords.ToArray();
-
-        // Expand route keyword to include the moons as compatible nouns
-        var compatibleNouns = routeKeyword.compatibleNouns.ToList();
-        compatibleNouns.Add(new CompatibleNoun { noun = dailyMoonKeyword, result = dailyMoonNode });
-        compatibleNouns.Add(new CompatibleNoun { noun = weeklyMoonKeyword, result = weeklyMoonNode });
-        routeKeyword.compatibleNouns = compatibleNouns.ToArray();
-
-        // Add the moon names to the Moons TerminalKeyword
+        // Add extra newline for spacing
         __instance.terminalNodes.allKeywords.First(keyword => keyword.name == "Moons").specialKeywordResult
-                .displayText +=
-            "* " + Plugin.DailyMoonName + " [planetTime]\n* " + Plugin.WeeklyMoonName + " [planetTime]\n\n";
+            .displayText += "\n";
     }
 }
